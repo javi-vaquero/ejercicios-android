c5d2dfddc27fb3618b6bee606e8e32ce189f5259
Javier Vaquero <javi.vaquero@gmail.com>
2015-03-24 19:27:03 +0100

    Broadcast

---
+++ IntentApplication/app/src/main/java/com/javivaquero/intentapplication/receiver/ConnectionReceiver.java
@@ -0,0 +1,26 @@
+package com.javivaquero.intentapplication.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.util.Log;
+
+public class ConnectionReceiver extends BroadcastReceiver {
+    private final String RECEIVER = "RECEIVER";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(RECEIVER, "ConnectionReceiver onReceive()");
+        Log.d(RECEIVER, "ACTION: " + intent.getAction());
+        if(intent.getAction().equals(Intent.ACTION_AIRPLANE_MODE_CHANGED)){
+
+            //Do something
+        }
+        else if(intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)){
+
+
+        }
+
+    }
+}
---
+++ IntentApplication/app/src/main/java/com/javivaquero/intentapplication/receiver/TelephonyReceiver.java
@@ -0,0 +1,17 @@
+package com.javivaquero.intentapplication.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class TelephonyReceiver extends BroadcastReceiver {
+
+    private final String RECEIVER = "RECEIVER";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(RECEIVER, "TelephonyReceiver onReceive()");
+        Log.d(RECEIVER, "ACTION: " + intent.getAction());
+    }
+}
